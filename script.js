6137a00e339a5a7138f0e0f2ac91a5f0cf6beb48
Navin Malakar <161839292+NavinMalakar2@users.noreply.github.com>
2025-07-16 23:07:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-	let s1lower=s1.toLowerCase();
-	let s2lower=s2.toLowerCase();
-	return s1lower.indexof(s2lower);
+  // Convert both strings to lowercase
+  let s1lower = s1.toLowerCase();
+  let s2lower = s2.toLowerCase();
+  return s1lower.indexOf(s2lower); // Fixed the typo: indexof ➜ indexOf
 }
 
 // Please do not change the code below
